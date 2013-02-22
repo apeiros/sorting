2811dcc8886bca3407bfe9fb759d5ef34b802cb5
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2013-02-22 23:06:22 +0100

    Added documentation for Sorting::Helpers::MethodDefinitions constant.

--- lib/sorting/helpers.rb
+++ lib/sorting/helpers.rb
@@ -12,7 +12,14 @@ module Sorting
   # This module provides convenience methods to create Sorting::Ascending and
   # Sorting::Descending instances.
   module Helpers
+
+    # The proc with the method definitions of asc and desc.
+    # They are in a proc so the proc can be class_eval'ed in Sorting and Kernel
+    # to avoid an issue with methods of modules which are included in another
+    # module not being properly shared
+    #
     MethodDefinitions = proc {
+
       # Convenience method to create a Sorting::Ascending instance.
       # Works exactly the same as {Sorting::Ascending#initialize}
       def asc(*args, &lazy)
@@ -27,6 +34,7 @@ module Sorting
       end
       private :desc
     }
+
     class_eval(&MethodDefinitions)
   end
 end
