9c84cbb5dcbb1ea8f42064119ceb800344c02fb1
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2013-02-25 11:04:43 +0100

    Fixed a bug with coverage reporting - seems simplecov must be loaded first, not just before the code under test.

--- test/runner.rb
+++ test/runner.rb
@@ -1,16 +1,19 @@
 # run with `ruby test/runner.rb`
 # if you only want to run a single test-file: `ruby test/runner.rb testfile.rb`
 
+if ENV['COVERAGE'] # IMPORTANT! It seems simplecov must be required before anything else, not just the code-under-test
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter "/test/"
+    add_filter "/examples/"
+  end
+end
+
 $LOAD_PATH << File.expand_path('../../lib', __FILE__)
 $LOAD_PATH << File.expand_path('../../test/lib', __FILE__)
 
 require 'test/unit'
 require 'sorting_test/helper'
-
-if ENV['COVERAGE']
-  require 'simplecov'
-  SimpleCov.start
-end
 
 units = ARGV.empty? ? Dir["#{TEST_DIR}/unit/**/*.rb"] : ARGV
 
